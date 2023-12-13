85781288ac12b6b6168a80b64adc6c5613532472
guille <129344574+guilleixy@users.noreply.github.com>
2023-12-13 09:20:01 +0100

    added metadata

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 
 import { Home, About, Projects, Contact } from './pages';
 import Navbar from './components/Navbar';
+import { Helmet } from "react-helmet";
+
 
 const App = () => {
     return(
         <main className='h-full'>
         {/* <main className='bg-slate-300/20 h-full'> */}
+            <Helmet>
+                <title>Guillermo Bernal</title>
+                <meta name="description" content='portfolio for the web developer Guillermo Bernal'/>
+                <meta name="keywords" content='web developer, portfolio, Guillermo Bernal, react, three.js, frontend'/>
+            </Helmet>
             <Router>
                 <Navbar />
                 <Routes>
